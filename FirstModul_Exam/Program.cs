802446dff231847a2e1d55271ad3ef14a8fd17db
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2024-11-30 11:01:43 +0500

    First modul exam uploaded

--- FirstModul_Exam/Program.cs
+++ FirstModul_Exam/Program.cs
@@ -1,10 +1,52 @@
 ﻿namespace FirstModul_Exam
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+        public static int TwoRoomsCount(List<int> numbers)
+        {
+            var count = 0;
+            foreach (var number in numbers)
+            {
+                if (number > 9 && number < 100)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static string RemoveEndsTwoChar(string text)
+        {
+            var length = text.Length;
+            var result = text.Remove(length - 2);
+            return result;
+        }
+        public static bool CheckIsUpperOneElements(List<string> words)
+        {
+            var checkingWord = true;
+            foreach (var word in words)
+            {
+                if (char.IsLower(word[0]) || char.IsDigit(word[0]) || !char.IsLetter(word[0]))
+                {
+                    checkingWord = false;
+                    break;
+                }
+            }
+            return checkingWord;
+        }
+        public static bool CheckingQovun(string text)
+        {
+            var startWith = text.StartsWith("qovun");
+            var endWith = text.EndsWith("qovun");
+            var result = false;
+            if (startWith is true && endWith is true)
+            {
+                result = true;
+            }
+            return result;
         }
     }
 }
---
+++ FirstModul_Exam/Services/PhoneService.cs
@@ -0,0 +1,63 @@
+﻿using FirstModul_Exam.Models;
+
+namespace FirstModul_Exam.Services;
+
+public class PhoneService
+{
+    private List<Phone> phones;
+    public PhoneService()
+    {
+        phones = new List<Phone>();
+    }
+    public Phone AddPhone(Phone phone)
+    {
+        phone.Id = Guid.NewGuid();
+        phones.Add(phone);
+        return phone;
+    }
+    public Phone GetById(Guid phoneId)
+    {
+        foreach (var phone in phones)
+        {
+            if (phone.Id == phoneId)
+            {
+                return phone;
+            }
+        }
+        return null;
+    }
+    public bool DeletePhone(Guid phoneId)
+    {
+        var boolValue = false;
+        var result = GetById(phoneId);
+        if (result == null)
+        {
+            return boolValue;
+        }
+        foreach (var phone in phones)
+        {
+            if (phone.Id == phoneId)
+            {
+                phones.Remove(phone);
+                boolValue = true;
+                return boolValue;
+            }
+        }
+        return boolValue;
+    }
+    public bool UpdatePhone(Phone phone)
+    {
+        var phoneFromDb = GetById(phone.Id);
+        if (phoneFromDb == null)
+        {
+            return false;
+        }
+        var index = phones.IndexOf(phone);
+        phones[index] = phone;
+        return true;
+    }
+    public List<Phone> GetAllPhones()
+    {
+        return phones;
+    }
+}
